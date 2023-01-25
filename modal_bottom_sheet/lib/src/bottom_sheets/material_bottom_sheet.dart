630cdfb73ce3b1b4d9929e57a4497584538c8010
Orgil Tse <orgilts603@gmail.com>
2023-01-25 17:46:21 +0800

    fix some any bugs

--- modal_bottom_sheet/lib/src/bottom_sheets/material_bottom_sheet.dart
+++ modal_bottom_sheet/lib/src/bottom_sheets/material_bottom_sheet.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart' as BottomSheet;
 import 'dart:async';
 
 /// Shows a modal material design bottom sheet.
@@ -25,7 +25,7 @@ Future<T?> showMaterialModalBottomSheet<T>({
   assert(debugCheckHasMediaQuery(context));
   assert(debugCheckHasMaterialLocalizations(context));
   final result = await Navigator.of(context, rootNavigator: useRootNavigator)
-      .push(ModalBottomSheetRoute<T>(
+      .push(BottomSheet.ModalBottomSheetRoute(
     builder: builder,
     closeProgressThreshold: closeProgressThreshold,
     containerBuilder: _materialContainerBuilder(
@@ -51,7 +51,8 @@ Future<T?> showMaterialModalBottomSheet<T>({
 }
 
 //Default container builder is the Material Appearance
-WidgetWithChildBuilder _materialContainerBuilder(BuildContext context,
+BottomSheet.WidgetWithChildBuilder _materialContainerBuilder(
+    BuildContext context,
     {Color? backgroundColor,
     double? elevation,
     ThemeData? theme,
